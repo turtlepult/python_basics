d39ecbaae91f12f18a20dd63995645bab2c9adc3
TurtlePult <ivan_ladiygin@mail.ru>
2023-03-25 18:09:17 +0500

    task_1 compl

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -1,8 +1,16 @@
-"""
-1. Реализовать скрипт, в котором должна быть предусмотрена функция
-расчета заработной платы сотрудника. В расчете необходимо использовать формулу:
-(выработка в часах*ставка в час) + премия.
+input_value_mn = [int(x) for x in input("aksjdbas: ").split()]
+m = input_value_mn[0]
+n = input_value_mn[1]
 
-Для выполнения расчета для конкретных значений
-необходимо запускать скрипт с параметрами.
-"""
+set_1 = set([int(x) for x in input("123213: ").split()])
+set_2 = set([int(x) for x in input("1231232: ").split()])
+while len(set_1) > m or len(set_2) > n:
+    print("Вы ввели больше количество элементов, чем задали:")
+    set_1 = set([int(x) for x in input("123213: ").split()])
+    set_2 = set([int(x) for x in input("1231232: ").split()])
+print(set_1)
+print(set_2)
+res_set = list(set_1 & set_2)
+res_set.sort()
+for i in res_set:
+    print(i, end=' ')
