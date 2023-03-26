684be2cdb8a8f19d3d67f74820611cd252a5e285
TurtlePult <ivan_ladiygin@mail.ru>
2023-03-26 17:41:39 +0500

    task_1 comp

---
+++ Урок 5. Практическое задание/task_1.py
@@ -0,0 +1,33 @@
+def calc():
+    operation = input("Введите операцию(+, -, *, / или 0 для выхода из программы): ")
+    if operation == "+":
+        num_1 = int(input("Введите первое число: "))
+        num_2 = int(input("Введите второе число: "))
+        print(f"Ваш результат: {num_1 + num_2}")
+        return calc()
+    elif operation == "-":
+        num_1 = int(input("Введите первое число: "))
+        num_2 = int(input("Введите второе число: "))
+        print(f"Ваш результат: {num_1 - num_2}")
+        return calc()
+    elif operation == "*":
+        num_1 = int(input("Введите первое число: "))
+        num_2 = int(input("Введите второе число: "))
+        print(f"Ваш результат: {num_1 * num_2}")
+        return calc()
+    elif operation == "/":
+        num_1 = int(input("Введите первое число: "))
+        num_2 = int(input("Введите второе число: "))
+        if num_2 == 0:
+            print("на ноль делить нельзя!!!")
+            return calc()
+        else:
+            print(f"Ваш результат: {num_1 / num_2}")
+    elif operation == "0":
+        return
+    else:
+        print("Вы ввели не верную операцию!!!")
+        return calc()
+
+
+calc()
