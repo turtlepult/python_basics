82a3aece122282c3e2fdd4b407a04ca866256c1c
TurtlePult <ivan_ladiygin@mail.ru>
2023-03-26 19:33:42 +0500

    task_26 compl

---
+++ Урок 5. Практическое задание/task_26.py
@@ -0,0 +1,13 @@
+def degree(a, b, res=1, i=1):
+    if b<i:
+        print(res)
+        return
+    else:
+        res *= a
+        i += 1
+        return degree(a,b,res,i)
+
+
+a = int(input("веддите число основание: "))
+b = int(input("веддите число показатель степени: "))
+degree(a, b)
