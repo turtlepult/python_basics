cc80c8e3f8ae501f520dcf90753b5fa73141cd13
TurtlePult <ivan_ladiygin@mail.ru>
2023-03-26 18:13:03 +0500

    task_2 compl

---
+++ Урок 5. Практическое задание/task_2.py
@@ -0,0 +1,16 @@
+def parity_counter(num, count):
+    last_number = num % 10
+    num = num // 10
+    if last_number % 2 == 0:
+        count[1] += 1
+    elif last_number % 2 != 0:
+        count[0] += 1
+    if num == 0:
+        print(f"[нечетные, четные]{count}")
+        return
+    else:
+        return parity_counter(num, count)
+
+counter = [0]*2
+user_number = int(input("Введите число:"))
+parity_counter(user_number, counter)
