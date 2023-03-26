b23eb8b592daff982af68a62c6f9885f32610a74
TurtlePult <ivan_ladiygin@mail.ru>
2023-03-26 18:59:55 +0500

    task_4 compl

---
+++ Урок 5. Практическое задание/task_4.py
@@ -0,0 +1,12 @@
+def revers_number(num, rev):
+    if num == 0:
+        print(rev)
+        return
+    else:
+        rev += str(num % 10)
+        return revers_number(num // 10, rev)
+
+
+user_number = int(input("Введите число: "))
+rev = ""
+revers_number(user_number, rev)
