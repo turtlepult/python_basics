b4e567ea6563b6195be30d537a0f802757f24f14
TurtlePult <ivan_ladiygin@mail.ru>
2023-03-26 19:38:26 +0500

    task_28 compl

---
+++ Урок 5. Практическое задание/task_28.py
@@ -0,0 +1,13 @@
+def sum(a, b):
+    if b == 0:
+        print(a)
+        return
+    else:
+        a += 1
+        b -= 1
+        return sum(a, b)
+
+
+a = int(input("a: "))
+b = int(input("b: "))
+sum(a, b)
