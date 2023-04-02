ae864ed297192ecbd044c40261ae00015de24082
TurtlePult <ivan_ladiygin@mail.ru>
2023-04-02 18:16:03 +0500

    task_2 compl

---
+++ Урок 7. Практическое задание/task_2.py
@@ -0,0 +1,14 @@
+class Road():
+    _length = int
+    _width = int
+
+    def calculation_asphalt(self, *args):
+        global _length
+        global _width
+        _length = 5000
+        _width = 20
+        print(f"На дорогу потребуется {(_length * _width * args[0] * args[1]) / 1000} тонн")
+
+
+calc = Road()
+calc.calculation_asphalt(25, 0.05)
