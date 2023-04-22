afbf6a2dd144e87987545f6bb5b7d3ec81c5b9f1
TurtlePult <ivan_ladiygin@mail.ru>
2023-04-22 07:31:01 +0500

    task 1 compl

---
+++ Урок 9. Работа с табличными данными/task_1.py
@@ -0,0 +1,41 @@
+class OneOf():
+
+    def __init__(self, *options):
+        self.options = set(options)
+
+    def __set__(self, obj, value):
+        if value not in self.options:
+            raise ValueError(
+                f'"{value}" должно быть одним из {self.options}')
+
+
+class IsNumber():
+
+    def __set__(self, instance, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError(f'{value} не число')
+        self.value = value
+
+    def __get__(self, instance, cls):
+        return self.value
+
+
+class Radiobutton():
+
+    current = OneOf('1-й вариант', '2-й вариант', '3-й вариант')
+
+    def __init__(self, default) -> None:
+        self.current = default
+
+
+class Number():
+
+    value = IsNumber()
+
+
+
+num = Number()
+num.value = 1
+print(num.value)
+ num.value = '1'
+ print(num.value)
